a5c5c0a0a77a2bc0a6ebb9e9b421cd2fd94e0be1
raninNa <raninnassra@gmail.com>
2020-08-31 19:00:41 +0300

    - added new parameters

--- app/src/main/java/com/example/pairingclient/RegisterInfoActivity.java
+++ app/src/main/java/com/example/pairingclient/RegisterInfoActivity.java
@@ -14,10 +14,95 @@ public class RegisterInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_info);
         Spinner spinnerGender =  (Spinner) findViewById(R.id.spinnerGender);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.gender_array, android.R.layout.simple_spinner_item);
-        spinnerGender.setAdapter(adapter);
+        spinnerGender.setAdapter(adapter1);
         spinnerGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(getContext(), "this is " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Spinner spinnerLocation =  (Spinner) findViewById(R.id.spinnerLocation);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.location_array, android.R.layout.simple_spinner_item);
+        spinnerLocation.setAdapter(adapter2);
+        spinnerLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(getContext(), "this is " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Spinner spinnerWorkingWay =  (Spinner) findViewById(R.id.spinnerWorkingWay);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.working_way_array, android.R.layout.simple_spinner_item);
+        spinnerWorkingWay.setAdapter(adapter3);
+        spinnerWorkingWay.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(getContext(), "this is " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Spinner spinnerMeetings =  (Spinner) findViewById(R.id.spinnerMeetings);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.meetings_array, android.R.layout.simple_spinner_item);
+        spinnerMeetings.setAdapter(adapter4);
+        spinnerMeetings.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(getContext(), "this is " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Spinner spinnerPreferredGender =  (Spinner) findViewById(R.id.spinnerPreferredGender);
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
+                R.array.preffered_gender_array, android.R.layout.simple_spinner_item);
+        spinnerPreferredGender.setAdapter(adapter5);
+        spinnerPreferredGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(getContext(), "this is " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Spinner spinnerWorkingTime =  (Spinner) findViewById(R.id.spinnerWorkingTime);
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,
+                R.array.working_time_array, android.R.layout.simple_spinner_item);
+        spinnerWorkingTime.setAdapter(adapter6);
+        spinnerWorkingTime.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
