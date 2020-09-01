71fbf24c53a52bfb54df904206ab02b99ea4537f
yuosifhanna95 <41512322+yuosifhanna95@users.noreply.github.com>
2020-09-01 10:15:12 +0300

    Update database connection, register

---
+++ app/src/main/java/com/example/pairingclient/Globals.java
@@ -0,0 +1,7 @@
+package com.example.pairingclient;
+
+public class Globals {
+    public static String faculty;
+    public static String course;
+    public static String workType;
+}
---
+++ app/src/main/java/com/example/pairingclient/RegisterUserReq.java
@@ -0,0 +1,47 @@
+package com.example.pairingclient;
+
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class RegisterUserReq extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "https://pairingapp.000webhostapp.com/RegisterUser.php";
+    private Map<String, String> params;
+
+    public RegisterUserReq(String name, String gender,String location, int age, String phone, String email, String year, String gradeAverage, String workPlan, String meeting, String prefGen, String workHours, Boolean iLocation,
+                           Boolean iGrade, String faculty, String course, String workType, String dbname, String dbuser, String dbpass, Response.Listener<String> listener) {
+        super(Request.Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("dbname", dbname);
+        params.put("dbuser", dbuser);
+        params.put("dbpass", dbpass);
+        params.put("name", name);
+        params.put("gender", gender);
+        params.put("location", location);
+        params.put("age", age + "");
+        params.put("phone", phone);
+        params.put("email", email);
+        params.put("year", year);
+        params.put("gradeAverage", gradeAverage);
+        params.put("workPlan", workPlan );
+        params.put("meeting", meeting);
+        params.put("prefGen", prefGen);
+        params.put("workHours", workHours);
+        params.put("iLocation", iLocation.toString());
+        params.put("iGrade", iGrade.toString());
+        params.put("faculty", faculty);
+        params.put("course", course);
+        params.put("workType", workType);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
