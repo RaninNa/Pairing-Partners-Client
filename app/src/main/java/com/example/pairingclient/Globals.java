ce720ee91dc4cb13c132b85291571a757dfbd274
yuosifhanna95 <41512322+yuosifhanna95@users.noreply.github.com>
2020-09-09 12:22:25 +0300

    added location pick up

--- app/src/main/java/com/example/pairingclient/Globals.java
+++ app/src/main/java/com/example/pairingclient/Globals.java
@@ -1,8 +1,11 @@
 package com.example.pairingclient;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Globals {
     public static String faculty;
     public static String course;
     public static String workType;
     public static String global_user_name;
+    public static LatLng Location;
 }
