2595c64dd0f47c8b68eee09dd5b45a26003674b5
yuosifhanna95 <41512322+yuosifhanna95@users.noreply.github.com>
2020-09-09 12:25:00 +0300

    Merged after 1 confilict (Globals)
    Everthing is okay

--- app/src/main/java/com/example/pairingclient/Globals.java
+++ app/src/main/java/com/example/pairingclient/Globals.java
@@ -1,9 +1,12 @@
 package com.example.pairingclient;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Globals {
     public static String faculty;
     public static String course;
     public static String workType;
     public static String global_user_name;
     public static Partner[] partners;
+    public static LatLng Location;
 }
